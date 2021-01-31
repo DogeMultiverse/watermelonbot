09e95c0375723893b1e339d6674672bcbbe8d80d
alex.mindustry <alex.mindustry@gmail.com>
2021-01-31 23:35:34 +0800

    add testing condition

--- pyexamples/reply.py
+++ pyexamples/reply.py
@@ -6,11 +6,13 @@ import random
 from pyexamples import counting_bot
 
 
-def get_latest_exp(res):
+def get_latest_exp(res, convertedexp_doc):
     muuid = {}
     muuid_name = {}
     last_updated = {}
     exp_list = []
+    if convertedexp_doc is None:
+        convertedexp_doc = {}
     for doc in res:
         if doc["muuid"] not in muuid:
             muuid[doc["muuid"]] = {doc["servername"]: doc["EXP"]}
@@ -24,6 +26,8 @@ def get_latest_exp(res):
         else:
             muuid[doc["muuid"]][doc["servername"]] = doc["EXP"]
             last_updated[doc["muuid"]][doc["servername"]] = doc["date"]
+        if doc["muuid"] not in convertedexp_doc:
+            convertedexp_doc[doc["muuid"]] = {}
     str_builder = ""
     for muuid_i, exps in muuid.items():
         str_builder += "In Game Name: `" + muuid_name[muuid_i] + "`\n"  # +str(exps) +"\n"
@@ -35,22 +39,30 @@ def get_latest_exp(res):
                           'ALEX | TURBO PVP SERVER', "ALEX | PVP SERVER (ASIA)"]:
                 try:
                     exp = 0 if exp is None else exp
-                    serverstr = server[7:].replace(" SERVER", "") + (
+                    rservername = server[7:].replace(" SERVER", "")
+                    serverstr = rservername + (
                         " [TOP 1%]" if exp > 40000 else (" [TOP 10%]" if exp > 15000 else ""))
+                    if server[7:].replace(" SERVER", "") not in convertedexp_doc[muuid_i]:
+                        convertedexp_doc[muuid_i][rservername] = {"claimed": 0, "lcdate": None}
+                        claimed = 0
+                        lcdate = None
+                    else:
+                        claimed = convertedexp_doc[muuid_i][rservername]["claimed"]
+                        lcdate = convertedexp_doc[muuid_i][rservername]["lcdate"]
                     exp_builder += f"{exp:>6}  " + f"{serverstr:<21}" + \
-                                   last_updated[muuid_i][server].strftime("%Y-%m-%d %H:%M:%S") + "\n"
-                    muuid_exp_dict["servers"].append({"servername": server[7:].replace(" SERVER", ""),
-                                                      "exp": exp, "unclaimed": 0, "lcdate": None,
+                                   last_updated[muuid_i][server].strftime("%Y-%m-%d %H:%M") + f"   {claimed:<6}" + "\n"
+                    muuid_exp_dict["servers"].append({"servername": rservername,
+                                                      "exp": exp, "claimed": claimed, "lcdate": lcdate,
                                                       "lupdated": last_updated[muuid_i][server]
                                                       })
                 except Exception as e:
                     print(exp, server)
                     print(str(e))
         if len(exp_builder) > 0:
-            exp_builder = "```\n <EXP>  <SERVER>             <LAST UPDATED, UTC>\n" + exp_builder + "\n```"
+            exp_builder = "```\n <EXP>  <SERVER>             <LASTUPDATED,UTC>  <CLAIMED>\n" + exp_builder + "\n```"
             str_builder += exp_builder
             exp_list.append(muuid_exp_dict)
-    return str_builder, exp_list
+    return str_builder, exp_list, convertedexp_doc
 
 
 class MyClient(discord.Client):
@@ -111,8 +123,16 @@ class MyClient(discord.Client):
         elif message.content.startswith(prefix + 'hello'):
             await message.reply('Hello!', mention_author=True)
         elif message.content.startswith(prefix + "checkexp"):
+            if prefix == "t!" and message.author.id != 612861256189083669:
+                await message.channel.send("no testing for u")
+                return
             await message.channel.send('getting exp')
             cursor = self.expgains.find({"duuid": message.author.id})
+            convertedexp_doc = self.convertedexp.find_one({"duuid": message.author.id})
+            if convertedexp_doc is None:
+                self.convertedexp.insert_one({"duuid": message.author.id, "converted": None})
+            else:
+                convertedexp_doc = convertedexp_doc["converted"]
             res = []
             for i, cur in enumerate(cursor):
                 res.append(cur)
@@ -121,25 +141,28 @@ class MyClient(discord.Client):
                 await message.channel.send("User has no EXP or user not found.")
             else:
                 # await message.channel.send('user found')
-                str_builder, exp_dict = get_latest_exp(res)
+                str_builder, exp_dict, convertedexp_doc = get_latest_exp(res, convertedexp_doc)
                 if len(str_builder) > 0:
+                    self.convertedexp.find_one_and_replace({"duuid": message.author.id},
+                                                           {"duuid": message.author.id, "converted": convertedexp_doc})
                     await message.channel.send(str_builder)
                 else:
                     await message.channel.send("You have no exp. ;-;")
 
         elif message.content.startswith(prefix + "buyeffect"):
-            if message.author.id != 612861256189083669:
+            if prefix == "t!" and message.author.id != 612861256189083669:
                 await message.channel.send("coming soon")
                 return
-            # add a new collection to show how much was claimed
+            # checks for price n balance, if valid, make purchase, else error message
 
         elif message.content.startswith(prefix + "convertexp"):
-            if message.author.id != 612861256189083669:
+            if prefix == "t!" and message.author.id != 612861256189083669:
                 await message.channel.send("coming soon")
                 return
             # add a new collection to show how much was claimed # add last claimed time.
         elif message.content.startswith(prefix + "github"):
-            await message.channel.send("https://github.com/alexpvpmindustry/watermelonbot")
+            await message.channel.send("watermelonbot: https://github.com/alexpvpmindustry/watermelonbot\n" +
+                                       "lol bot: https://github.com/unjown/unjownbot")
 
         elif message.content.startswith(prefix):
             await message.channel.send("Unknown command, type `" + prefix + "help` for help.")
