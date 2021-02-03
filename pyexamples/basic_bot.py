e073dba265270aaeecc69690a1381ac344e3da8d
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-02-03 20:00:15 +0700

    Update basic_bot.py

--- pyexamples/basic_bot.py
+++ pyexamples/basic_bot.py
@@ -22,22 +22,22 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@client.command(name='eval', pass_context=True)
-@commands.is_owner()
-async def eval_(ctx, *, command):
-    res = eval(command)
-    if "bot_token", "mongo_key" in command:
-        return res = "oh no"
-        if inspect.isawaitable(res):
-            embed = discord.Embed(title="Developer Evaluation")
-            embed.add_field(field="Input", command)
-            embed.add_field("Output", await res)
-            await ctx.send(embed=embed)
-        else:
-            embed2 = discord.Embed(title="Developer Evaluation")
-            embed2.add_field(field="Input", command)
-            embed2.add_field("Output", res)
-            await ctx.send(embed=embed2)
+#@client.command(name='eval', pass_context=True)
+#@commands.is_owner()
+#async def eval_(ctx, *, command):
+#    res = eval(command)
+#    if "bot_token", "mongo_key" in command:
+#        return res = "oh no"
+#        if inspect.isawaitable(res):
+#            embed = discord.Embed(title="Developer Evaluation")
+#            embed.add_field(field="Input", command)
+#            embed.add_field("Output", await res)
+#            await ctx.send(embed=embed)
+#        else:
+#            embed2 = discord.Embed(title="Developer Evaluation")
+#            embed2.add_field(field="Input", command)
+#            embed2.add_field("Output", res)
+#            await ctx.send(embed=embed2)
     
 @bot.command()
 async def add(ctx, left: int, right: int):
