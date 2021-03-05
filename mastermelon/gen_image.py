f14950e3893764db2f60e4f79487a312adac85d3
alex.mindustry <alex.mindustry@gmail.com>
2021-03-06 01:56:48 +0800

    changed font

--- mastermelon/gen_image.py
+++ mastermelon/gen_image.py
@@ -32,7 +32,7 @@ async def getwelcomeimage(name: str = "nobody", avatar: discord.Asset = None):
     font_name = ImageFont.truetype("fonts/DejaVuSansMono.ttf", 14, )
     draw.text((50, 20), f"Certificate of Entry", (25, 25, 25), font=font_title, align="center")
     draw.text((49, 19), f"Certificate of Entry", (255, 215, 0), font=font_title, align="center")
-    draw.text((130, 49), "Alex Servers", fill=font_colors, font=ImageFont.truetype("calibril.ttf", 17))
+    draw.text((127, 48), "Alex Servers", fill=font_colors, font=ImageFont.truetype("arial.ttf", 16))
     draw.text((52, 50), f"Welcome to", fill=font_colors, font=font)
     name1 = [c for c in name if c.isalnum() or c in "_!@#$%^ &*()-=+~`'[],.?;:"]
     name1 = "".join(name1)
