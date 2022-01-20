6889eda9caa7ead10758ae473f685533d00ca519
alex.mindustry <alex.mindustry@gmail.com>
2022-01-20 21:24:53 +0800

    add checkax

--- mastermelon/melon.py
+++ mastermelon/melon.py
@@ -577,6 +577,20 @@ async def giveax(ctx: discord.ext.commands.Context, amount: int, user: discord.M
                            f"Now {old_val+amount}{ej.ax_emoji}.\nReason: {' '.join(reason)}")
 
 
+@bot.command(description="Check user's Ax. If no user is specified, check your own Ax",
+             brief="Admin Utility", help="[@user(Optional)]")
+async def checkax(ctx: discord.ext.commands.Context, *user):
+    if isinstance(user,type(None)):
+        user = ctx.author.id
+    old_val = ax.find_one({"duuid": user})
+    if isinstance(old_val,type(None)):
+        ax.insert_one({"duuid": user, "ax": 0})
+        old_val = 0
+    else:
+        old_val = old_val["ax"]
+    await ctx.channel.send(f"{user.mention} currently has {old_val}{ej.ax_emoji}.")
+
+
 def get_username(duuid: int):
     user = bot.get_user(duuid)
     if isinstance(user, type(None)):
