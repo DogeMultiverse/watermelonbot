7f30c762c983c9ad56744eca6835e5c6dc3822cb
alex.mindustry <alex.mindustry@gmail.com>
2022-03-24 23:23:20 +0800

    formatting

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import pymongo
 
 
-
-
-
-
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -18,9 +12,8 @@ from mastermelon import melon
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-    #guessing_game.runbot()
+    # guessing_game.runbot()
     try:
         melon.runbot()
     except RuntimeError or KeyboardInterrupt:
         print("exiting here")
-
