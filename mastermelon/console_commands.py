f35c99279a8533a31c45e48aa5b1ed72a350fd88
apmapmapm <alexpvpmindustry@github.com>
2023-08-30 16:20:47 +0800

    111

--- mastermelon/console_commands.py
+++ mastermelon/console_commands.py
@@ -57,9 +57,7 @@ async def readserver(ctx: commands.Context, serverid: int):
         fld=servfolders()[i]
         cmd =f'cat {fld}/screen_log.log'
         out,err = read_consoleoutput(host, cmd)
-        output = str(out)[2:-1].split("\\n") #"\n".join([str(out[-500:]).split("\n")])
-        #print(output)
-        #print("\n".join(output))
+        output = str(out[-3500:])[2:-1].split("\\n") 
         await ctx.channel.send( f"`{host}{port}` `{screen}`:\n"+ "\n".join(output))
         await ctx.channel.send(f"Completed reading for `{i}` `{host}{port}` `{screen}`")
     except Exception as e:
